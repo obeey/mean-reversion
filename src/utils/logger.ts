aed00ebaaafb344f553237d84e35452d3de6895c
ChenYongchu <chenyongchu@main99.com>
2024-09-30 21:38:35 +0800

    Log file move to log and log file name add timestamp.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,10 +2,25 @@ import { createLogger, format, transports, Logger, level } from "winston";
 
 const { combine, timestamp, printf } = format;
 
+function getCurrentFormattedDate(): string {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // 月份从0开始
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  return `${year}${month}${day}-${hours}${minutes}${seconds}`;
+}
+
 // 自定义日志格式
 const myFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
+
+const formattedDate = getCurrentFormattedDate();
 
 // 创建 Logger 实例
 const logger: Logger = createLogger({
@@ -18,8 +33,11 @@ const logger: Logger = createLogger({
   transports: [
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
-    new transports.File({ filename: "error.log", level: "error" }),
-    new transports.File({ filename: "combined.log" }),
+    new transports.File({
+      filename: `log/error_${formattedDate}.log`,
+      level: "error",
+    }),
+    new transports.File({ filename: `log/combined_${formattedDate}.log` }),
     new transports.Console({ level: "debug" }),
   ],
 });
