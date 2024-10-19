48cadbe2bc6e868c5c86fdeb43d2c5e03e419e4d
ChenYongchu <chenyongchu@main99.com>
2024-10-19 20:10:06 +0800

    remove hot tokens.

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,41 +1,6 @@
 import { Token } from "./token.d.js";
 
-let tokens: Token[] = [
-  {
-    name: "HANA",
-    address: "0xb3912b20b3abc78c15e85e13ec0bf334fbb924f7",
-    buyTimestamp: NaN,
-    historyPrice: [],
-    buyPrice: NaN,
-    highPrice: NaN,
-    buyAmount: 0,
-    buyEthCost: 0,
-    buyGasUsed: 0,
-    sellGasUsed: 0,
-    sellPending: false,
-    buyPending: false,
-    profit: 0,
-    tradeWin: 28,
-    tradeCount: 33,
-  },
-  {
-    name: "CSI",
-    address: "0x888c1a341ce9d9ae9c2d2a75a72a7f0d2551a2dc",
-    buyTimestamp: NaN,
-    historyPrice: [],
-    buyPrice: NaN,
-    highPrice: NaN,
-    buyAmount: 0,
-    buyEthCost: 0,
-    buyGasUsed: 0,
-    sellGasUsed: 0,
-    sellPending: false,
-    buyPending: false,
-    profit: 0,
-    tradeWin: 20,
-    tradeCount: 31,
-  },
-];
+let tokens: Token[] = [];
 
 let largeLossTokens: Token[] = [
   {
