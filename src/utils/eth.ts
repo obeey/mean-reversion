e6f3869a097f875b5fcbf8a81167d92871e7d063
ChenYongchu <chenyongchu@main99.com>
2024-11-07 08:28:22 +0800

    Change to mainnet.

--- src/utils/eth.ts
+++ src/utils/eth.ts
@@ -694,10 +694,10 @@ async function tradetest() {
 
 export default {
   getPrice: getRealPrice,
-  // buyToken: buyTokenMainnet,
-  // sellToken: sellTokenMainnet,
-  buyToken: buyTokenTest,
-  sellToken: sellTokenTest,
+  buyToken: buyTokenMainnet,
+  sellToken: sellTokenMainnet,
+  // buyToken: buyTokenTest,
+  // sellToken: sellTokenTest,
   getEthBalance,
   getErc20Balanceof,
   getPoolEthWei,
